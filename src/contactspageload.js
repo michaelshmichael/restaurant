1d8407f706d55b8f93948d4e1680e48159b8393a
logeybogey <michaeldcjohnson@gmail.com>
2020-11-01 15:58:13 +0600

    spruced up contacts page

--- src/contactspageload.js
+++ src/contactspageload.js
@@ -1,9 +1,32 @@
 const contactPageLoad = () => {
     let contentDiv = document.getElementById('content');
+
     let contactImage = document.createElement('img')
     contactImage.setAttribute('src','https://www.ipcaweb.org/images/IPC_Contact_Us.jpg');
     contactImage.setAttribute('id','contactImage')
     contentDiv.appendChild(contactImage)
+
+    let highVille = document.createElement('div')
+    highVille.setAttribute('id', 'highVilleContact')
+    let highVilleText = document.createElement('p')
+    highVilleText.setAttribute('class', 'contactText')
+    let highVillePhone = document.createElement('p')
+    highVilleText.textContent = 'Baitursynova 5/1'
+    highVillePhone.textContent = '+7 707 4534 922'
+    highVille.appendChild(highVilleText)
+    highVille.appendChild(highVillePhone)
+    contentDiv.appendChild(highVille)
+
+    let expo = document.createElement('div')
+    expo.setAttribute('id', 'expoContact')
+    let expoText = document.createElement('p')
+    expoText.setAttribute('class', 'contactText')
+    let expoPhone = document.createElement('p')
+    expoText.textContent = 'Expo Boulevard 44/7'
+    expoPhone.textContent = '+7 707 4433 212'
+    expo.appendChild(expoText)
+    expo.appendChild(expoPhone)
+    contentDiv.appendChild(expo)
 }
 
 export {contactPageLoad}
--- src/menupageload.js
+++ src/menupageload.js
@@ -1,7 +1,8 @@
 const menuPageLoad = () => {
     let contentDiv = document.getElementById('content');
     let menuImage = document.createElement('img')
-    menuImage.setAttribute('src','https://astana.restolife.kz/upload/information_system_26/5/4/0/group_5400/information_groups_property_12382.jpg') 
+    menuImage.setAttribute('src','https://astana.restolife.kz/upload/information_system_26/5/4/0/group_5400/information_groups_property_12382.jpg');
+    menuImage.setAttribute('id', 'menuImage') 
     contentDiv.appendChild(menuImage)
 }
 
